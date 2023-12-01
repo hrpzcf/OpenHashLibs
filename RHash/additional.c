d9412ef194f2ca4ae968ded5ac146644a49724e6
hrpzcf <pphrp@qq.com>
2023-12-01 14:45:00 +0800

    Add exports for ed2k, md4, and ripemd160 algorithms

--- RHash/additional.c
+++ RHash/additional.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
-#include "gost12.h"
+#include "ed2k.h"
+#include "md4.h"
+#include "ripemd-160.h"
 #include "sha256.h"
 #include "whirlpool.h"
 
@@ -15,6 +17,40 @@ INLINE void rhash_algo_delete(void *state)
     state = NULL;
 }
 
+// ed2k
+ed2k_ctx *rhash_ed2k_new(void)
+{
+    return malloc(sizeof(ed2k_ctx));
+}
+
+void rhash_ed2k_delete(ed2k_ctx *state)
+{
+    rhash_algo_delete(state);
+}
+
+// md4
+md4_ctx *rhash_md4_new(void)
+{
+    return malloc(sizeof(md4_ctx));
+}
+
+void rhash_md4_delete(md4_ctx *state)
+{
+    rhash_algo_delete(state);
+}
+
+// ripemd160
+ripemd160_ctx *rhash_ripemd160_new(void)
+{
+    return malloc(sizeof(ripemd160_ctx));
+}
+
+void rhash_ripemd160_delete(ripemd160_ctx *state)
+{
+    rhash_algo_delete(state);
+}
+
+// sha224
 sha256_ctx *rhash_sha224_new(void)
 {
     return malloc(sizeof(sha256_ctx));
@@ -25,6 +61,7 @@ void rhash_sha224_delete(sha256_ctx *state)
     rhash_algo_delete(state);
 }
 
+// whirlpool
 whirlpool_ctx *rhash_whirlpool_new(void)
 {
     return malloc(sizeof(whirlpool_ctx));
